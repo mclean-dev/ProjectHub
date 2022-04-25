d4269b4ef174d4cb04470b0b5bfefc18c64fa93d
John McLean <mclean.jb@gmail.com>
2022-04-25 19:08:37 -0400

    Add basic file structure

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import { Container } from '@mui/material'
 
 import Home from './components/Home/Home'
+import Auth from './components/Auth/Auth'
 
 function App() {
   const user = JSON.parse(localStorage.getItem('profile'))
@@ -13,6 +14,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Navigate replace to='/posts' />} />
           <Route path='/posts' element={<Home />} />
+          <Route path='/posts/search' element={<Home />} />
+          <Route path="/auth" element={() => (!user ? <Auth /> : <Navigate replace to="/posts" />)} />
+
         </Routes>
       </Container>
     </BrowserRouter>
