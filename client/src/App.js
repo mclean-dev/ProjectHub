6f34f28c18386d028c1f22058eed61ef0b6a6c40
John McLean <mclean.jb@gmail.com>
2022-04-25 17:37:53 -0400

    Add: Home component and router redirect.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { Container } from '@mui/material'
+
+import Home from './components/Home/Home'
 
 function App() {
+  const user = JSON.parse(localStorage.getItem('profile'))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Container maxWidth='xl'>
+        <Routes>
+          <Route path='/' element={<Navigate replace to='/posts' />} />
+          <Route path='/posts' element={<Home />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
